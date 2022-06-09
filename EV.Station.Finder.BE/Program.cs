a55700ed7b616c3be581f9c351e960a07e3daeac
alexander.brueck <alexander.brueck@credera.com>
2022-06-09 11:54:11 -0600

    Add data context options

--- EV.Station.Finder.BE/Program.cs
+++ EV.Station.Finder.BE/Program.cs
@@ -1,11 +1,15 @@
 ﻿global using StationAPI.Data;
+global using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
